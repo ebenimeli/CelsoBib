e866061ee0d327e90f6c82b7c3b82b7de64aafc1
Enrique Benimeli <ebenimeli@gmail.com>
2022-10-20 15:06:00 +0200

    Log test on server

--- update.sh
+++ update.sh
@@ -1,21 +1,30 @@
-SITEFOLDER=_/site/
-if test -f "$SITEFOLDER"; then
+#!/bin/bash
+BASE=/home/ebenimeli/GitHub/CelsoBib
+SITEFOLDER="$BASE/_site/"
+echo "Site folder: $SITEFOLDER"
+if [[ -d "$SITEFOLDER" ]]; then
     echo "Removing site..."
-    rm -r "$FILE"
+    rm -r "$SITEFOLDER"
+else
+    echo "No _site folder!"
 fi
-CACHE=./jekyll-cache/
-if test -f "$CACHE"; then
+CACHE="$BASE/.jekyll-cache/"
+echo "Cache folder: $CACHE"
+if [[ -d "$CACHE" ]]; then
    echo "Deleting cache..."
-   rm -r .jekyll-cache/
+   rm -r "$CACHE"
+else
+    echo "No cache folder!"
 fi
 
 echo "Pulling..."
 git pull
 
 echo "Building..."
-bundle exec jekyll build
+BUNDLE_GEMFILE=/home/ebenimeli/GitHub/CelsoBib/Gemfile bundle exec jekyll build /home/ebenimeli/GitHub/CelsoBib/
 
-WEB=/var/www/vhosts/ebenimeli.org/httpdocs/
-if test -f "$WEB"; then
-  cp -r _site/* "$WEB"
+WEB=/var/www/vhosts/ebenimeli.org/httpdocs
+if [[ -d "$WEB" ]]; then
+  echo "Copying web to $WEB ..."
+  cp -r /home/ebenimeli/GitHub/CelsoBib/_site/* "$WEB"
 fi
