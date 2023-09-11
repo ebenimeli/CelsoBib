0ae5392edf8e57b90f47d7dbdbffe1b7d103f65d
Enrique Benimeli <ebenimeli@gmail.com>
2023-09-11 15:49:01 +0200

    Site upload

---
+++ _site/python/scripts/plotwalk.py
@@ -0,0 +1,13 @@
+import pandas as pd
+data = pd.read_csv('data/walk.txt',sep='\s+',header=None)
+data = pd.DataFrame(data)
+
+import matplotlib.pyplot as plt
+x = data[0]
+y = data[1]
+plt.plot(x, y,'r--')
+plt.xlabel("días");
+plt.ylabel("km");
+
+plt.savefig('figures/walk.png')
+# plt.show()
