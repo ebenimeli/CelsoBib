503e97d615627b7e2c1e5d6a311a1cb2fb584d4e
Enrique Benimeli <ebenimeli@gmail.com>
2024-01-19 20:48:33 +0100

    Site update

--- _site/python/scripts/generate.sh
+++
@@ -1,8 +0,0 @@
-python3 plotelo.py
-cp figures/evolution.png ../../assets/images/pages/chess/
-python3 plotreading.py
-cp figures/reading.png ../../assets/images/pages/habits/
-python3 plotwalk.py
-cp figures/walk.png ../../assets/images/pages/habits/
-python3 plotwriting.py
-cp figures/writing.png ../../assets/images/pages/habits/
