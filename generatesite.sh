1bb5ac7b1585c44803f10797249d1c308be44c10
Enrique Benimeli <ebenimeli@gmail.com>
2025-08-12 00:09:37 +0200

    Fetch Mastodon disabled

--- generatesite.sh
+++ generatesite.sh
@@ -2,10 +2,10 @@
 set -e
 bundle exec jekyll build "$@"
 # Ejecutar fetch_toots.py como si estuviésemos dentro de python/
-(
-  cd python
+#(
+#  cd python
   # Si ya tiene shebang + +x:
-  ./fetch_toots.py
+#  ./fetch_toots.py
   # O, si prefieres invocar con python3:
   # python3 fetch_toots.py
-)
+#)
