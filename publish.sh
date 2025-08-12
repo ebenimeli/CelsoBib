52b07e4be7ed4c4d5cce9f51073887e13d6767fa
Enrique Benimeli <ebenimeli@gmail.com>
2025-08-12 16:43:12 +0200

    Wait message removed

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ set -e  # si un comando falla, se detiene el script
 # Función para esperar y mostrar cuenta atrás
 esperar() {
   for i in {10..1}; do
-    echo "⏳ Esperando $i segundos..."
+    # echo "⏳ Esperando $i segundos..."
     sleep 1
   done
 }
