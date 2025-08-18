f215f11c384455c10e625f76cbed129bf60f8369
Enrique Benimeli <ebenimeli@gmail.com>
2025-08-18 17:50:33 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
 $TO      = 'contacto@ebenimeli.org';
 $SUBJECT = 'Nuevo mensaje desde el formulario';
 $FROM    = 'no-reply@ebenimeli.org'; // Usa tu propio dominio (SPF/DKIM)
-$THANK_YOU_URL = 'pages/gracias.md';     // o null si quieres responder JSON
+$THANK_YOU_URL = 'pages/gracias.html';     // o null si quieres responder JSON
 
 // Solo POST
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
